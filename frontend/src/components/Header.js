c228917a446ff8d93bb25de0143696252732de36
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-06-26 18:36:06 +0100

    added errorMiddleware and the productRoutes, getting started with redux and managing the productList and productScreen with redux

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>PinazzaShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ΠnazzaShop</Navbar.Brand>
           </LinkContainer>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
+                  <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
+              <LinkContainer to='/login'>
                 <Nav.Link>
-                  <i className="fas fa-user"></i> Sign In
+                  <i className='fas fa-user'></i> Sign In
                 </Nav.Link>
               </LinkContainer>
             </Nav>
