1606880657acf83f221d7cb5eb711d894407659b
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-07-16 17:10:33 +0100

    backend profile user update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,4 +76,34 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile }
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    // Before updating the password, let's check first if it's sent
+    if (req.body.password) {
+      user.password = req.body.password
+      // it's gonna be encrypted autmatically because of what we did in the userModel
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
