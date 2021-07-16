bc9c7b89a6534c287730fd9d056d49793e851977
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-07-16 22:50:29 +0100

    updating the profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import {
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -7,6 +10,9 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -53,7 +59,7 @@ export const register = (name, email, password) => async (dispatch) => {
     // Dispatch User Register Request
     dispatch({ type: USER_REGISTER_REQUEST })
 
-    // After the request we wanna dispatch the user login, let's create a config object because when we're actually sending data, we wanna send in the headers a content type of application/json
+    // After the request we wanna dispatch the user register, let's create a config object because when we're actually sending data, we wanna send in the headers a content type of application/json
     const config = {
       // This is also where we pass the token for our protected route
       headers: {
@@ -85,6 +91,78 @@ export const register = (name, email, password) => async (dispatch) => {
     // Dispatch User Register fail
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    // Dispatch User Details Request
+    dispatch({ type: USER_DETAILS_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+    // After the request we wanna dispatch the user details, let's create a config object because when we're actually sending data, we wanna send in the headers a content type of application/json
+    const config = {
+      // This is also where we pass the token for our protected route
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/users/${id}`, config)
+
+    // Dispatch User details Success
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    // Dispatch User Register fail
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    // Dispatch User Update Profile Request
+    dispatch({ type: USER_UPDATE_PROFILE_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+    // After the request we wanna dispatch the user details, let's create a config object because when we're actually sending data, we wanna send in the headers a content type of application/json
+    const config = {
+      // This is also where we pass the token for our protected route
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/users/profile`, user, config)
+
+    // Dispatch User details Success
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    // Dispatch User Register fail
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,121 @@
+// This will be a form, and when we have a form, usually our form fields are gonna be part of the component state, so we do need "useState" and "useEffect".
+import React, { useState, useEffect } from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
+
+const ProfileScreen = ({ location, history }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector((state) => state.userDetails)
+  const { loading, error, user } = userDetails
+
+  // Check if the user is logged in to be able to access this page.
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  // Showing a message when the update is successful.
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfile
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login')
+    } else {
+      if (!user.name) {
+        dispatch(getUserDetails('profile'))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+      }
+    }
+  }, [dispatch, history, userInfo, user])
+
+  const submitHandler = (e) => {
+    // we add preventDefault so the page doesn't actually refresh
+    e.preventDefault()
+
+    // Check the password
+    if (password !== confirmPassword) {
+      setMessage('Passwords Do Not Match')
+    } else {
+      // Dispatch Update Profile
+      dispatch(updateUserProfile({ id: user._id, name, email, password }))
+    }
+  }
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {/* Showing a message that the update is successful */}
+        {success && (
+          <Message variant='success'>Profile Updated Successfully</Message>
+        )}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter Your Name'
+              // The "value" is gonna be the name from the state
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter Your Email'
+              // The "value" is gonna be the email from the state
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter Your Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='confirmPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Confirm Your Password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type='submit' variant='primary' className='my-3'>
+            Update
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+      </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen
