6d6dbe19385133d2eb59e7408ce875fa086c8a35
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-08-02 20:26:37 +0100

    delete a user for admin functionnality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -115,4 +115,27 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users)
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
+// @desc    Delete a user
+// @route   DELETE /api/users/:id
+// @access  Private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await user.remove()
+    res.status(204)
+    res.json({ message: 'User Removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+}
