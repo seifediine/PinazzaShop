222f13972893ee3b070220fef58eae785729d738
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-08-06 14:11:24 +0100

    add and update product functionality and screen

--- frontend/src/constants/productConstants.js
+++ frontend/src/constants/productConstants.js
@@ -1,4 +1,4 @@
-// The first  step in implemnting the state management with Redux is to create constants that we will export to the respective reducer
+// The first step in implemnting the state management with Redux is to create constants that we will export to the respective reducer
 
 export const PRODUCT_LIST_REQUEST = 'PRODUCT_LIST_REQUEST'
 export const PRODUCT_LIST_SUCCESS = 'PRODUCT_LIST_SUCCESS'
@@ -11,3 +11,13 @@ export const PRODUCT_DETAILS_FAIL = 'PRODUCT_DETAILS_FAIL'
 export const PRODUCT_DELETE_REQUEST = 'PRODUCT_DELETE_REQUEST'
 export const PRODUCT_DELETE_SUCCESS = 'PRODUCT_DELETE_SUCCESS'
 export const PRODUCT_DELETE_FAIL = 'PRODUCT_DELETE_FAIL'
+
+export const PRODUCT_CREATE_REQUEST = 'PRODUCT_CREATE_REQUEST'
+export const PRODUCT_CREATE_SUCCESS = 'PRODUCT_CREATE_SUCCESS'
+export const PRODUCT_CREATE_FAIL = 'PRODUCT_CREATE_FAIL'
+export const PRODUCT_CREATE_RESET = 'PRODUCT_CREATE_RESET'
+
+export const PRODUCT_UPDATE_REQUEST = 'PRODUCT_UPDATE_REQUEST'
+export const PRODUCT_UPDATE_SUCCESS = 'PRODUCT_UPDATE_SUCCESS'
+export const PRODUCT_UPDATE_FAIL = 'PRODUCT_UPDATE_FAIL'
+export const PRODUCT_UPDATE_RESET = 'PRODUCT_UPDATE_RESET'
