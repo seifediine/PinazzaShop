54878dea378cadd481741fa1f681be335990da0c
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-08-07 22:59:01 +0100

    adding search product functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 // We gonna have to bring our userLogin state into the Header component, so we import useDispatch and useSelector from redux
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 
 const Header = () => {
@@ -23,6 +25,7 @@ const Header = () => {
 
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
