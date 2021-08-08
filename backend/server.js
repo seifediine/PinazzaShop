c405a6bd086cd1859fb62766aa292500757642da
Seifeddine Elkorichi <seifeddine.elkorichi@hotmil.com>
2021-08-09 00:51:07 +0100

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running!')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -38,10 +34,22 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 // Make the uploads folder static
-
 // __dirname is available in common js and not ES modules so we mimic that behavior
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+// Prepare for deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'fronend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running!')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
